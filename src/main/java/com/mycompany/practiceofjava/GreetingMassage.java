d06222e5d0f8554a615d704ef68b15b24024a716
inaya-treva <samieajahaneva9543@gmail.com>
2025-07-08 22:19:16 +0600

    summation

---
+++ src/main/java/com/mycompany/practiceofjava/GreetingMassage.java
@@ -0,0 +1,15 @@
+
+package com.mycompany.practiceofjava;
+
+import java.time.DayOfWeek;
+import java.util.Scanner;
+
+public class GreetingMassage {
+    public static void main(String[] args) {
+        try(Scanner input = new Scanner(System.in)){
+            String name;
+            System.out.print("Enter your name : ");
+            name = input.nextLine();
+            System.out.println("Hello "+name+", have a good day. ");
+    }
+}
