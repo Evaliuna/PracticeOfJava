b2fe26624802f636030f3fc647c342dffa393cc1
inaya-treva <samieajahaneva9543@gmail.com>
2025-07-10 06:07:04 +0600

    Detect Int

---
+++ src/main/java/com/mycompany/practiceofjava/DetectInt.java
@@ -0,0 +1,23 @@
+
+package com.mycompany.practiceofjava;
+
+import java.util.Scanner;
+
+public class DetectInt {
+    public static void main(String[] args){
+        try(Scanner input = new Scanner(System.in)){
+                double num;
+                System.out.println("Enter the number : ");
+                num = input.nextDouble();
+                int check = (int)num;
+               if(check == num) {
+                    System.out.println("It is an Integer.");
+               }
+               else{
+                System.out.println("No, it's not.");
+               }
+                
+        }
+        
+    }
+}
