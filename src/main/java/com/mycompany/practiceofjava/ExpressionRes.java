b3ec2ba5d1664c86e3a4396b476df38cacdd0c8d
inaya-treva <samieajahaneva9543@gmail.com>
2025-07-10 08:37:54 +0600

    Expression Result

--- src/main/java/com/mycompany/practiceofjava/ExpressionRes.java
+++ src/main/java/com/mycompany/practiceofjava/ExpressionRes.java
@@ -5,5 +5,8 @@ public class ExpressionRes {
     public static void main(String[] args) {
         float a = 7/4*9/2;//left to right as smae precedence
         System.out.println(a);
+        int x = 7;
+        int b = 7*49/7+35/7;
+        System.out.println(b);
     }
 }
